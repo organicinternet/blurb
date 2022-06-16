44794be75f6411b259604bde8a9d18091133e8df
kejincan <kejincan@digitsuper.com>
2022-06-17 02:00:12 +0800

    :building_construction: Refactored history_request

--- lib/blurb/history_request.rb
+++ lib/blurb/history_request.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-require 'blurb/request_collection'
+require "blurb/request_collection"
 
 class Blurb
   class HistoryRequest < RequestCollection
-    FROM_DATE = (DateTime.now - 30).strftime('%Q')
-    TO_DATE = DateTime.now.strftime('%Q')
+    FROM_DATE = DateTime.now - 30
+    TO_DATE = DateTime.now
     MAX_COUNT = 200
     MIN_COUNT = 50
-    FILTERS = [].freeze
-    PARENT_CAMPAIGN_ID = nil
 
     def initialize(base_url:, headers:)
       @base_url = base_url
@@ -17,11 +15,12 @@ class Blurb
     end
 
     def retrieve(
-      campaign_ids:, from_date: FROM_DATE,
+      from_date: FROM_DATE,
       to_date: TO_DATE,
-      filters: FILTERS,
-      parent_campaign_id: PARENT_CAMPAIGN_ID,
-      count: MAX_COUNT
+      event_types:,
+      count: MAX_COUNT,
+      sort_direction: "DESC",
+      page_offset: 0
     )
 
       count = MIN_COUNT if count < MIN_COUNT
@@ -29,28 +28,20 @@ class Blurb
 
       payload = {
         sort: {
-          key: 'DATE',
-          direction: 'ASC'
+          key: "DATE",
+          direction: sort_direction
         },
-        fromDate: from_date.to_i,
-        toDate: to_date.to_i,
-        eventTypes: {
-          CAMPAIGN: {
-            eventTypeIds: campaign_ids
-          }
-        },
-        count:
+        fromDate: from_date.strftime("%Q").to_i,
+        toDate: to_date.strftime("%Q").to_i,
+        eventTypes: event_types,
+        count: count,
+        pageOffset: page_offset
       }
 
-      payload[:eventTypes][:CAMPAIGN].merge!({ filters: }) if filters.present?
-      if parent_campaign_id.present?
-        payload[:eventTypes][:CAMPAIGN].merge!({ parents: [{ campaignId: parent_campaign_id }] })
-      end
-
       execute_request(
-        api_path: '/history',
+        api_path: "/history",
         request_type: :post,
-        payload:
+        payload: payload
       )
     end
   end
